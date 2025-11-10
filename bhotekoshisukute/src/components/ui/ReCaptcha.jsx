7dd13676bd1052cb19e64818355195a6ae4d3637
itsmesajan <itsmesajanmaharjan@gmail.com>
2025-11-10 17:27:25 +0545

    [UPDATE] recaptcha token and added font

--- bhotekoshisukute/src/components/ui/ReCaptcha.jsx
+++ bhotekoshisukute/src/components/ui/ReCaptcha.jsx
@@ -4,7 +4,7 @@ import ReCAPTCHA from "react-google-recaptcha";
 const ReCaptcha = ({ onChange }) => {
   return (
     <ReCAPTCHA
-      sitekey="6Lf1CysqAAAAAIgmN0_09HdspdNsgi6359cuvp4j"
+      sitekey="6LflBQgsAAAAAEJcgi4mwrDVu_Ng1Lbw1TZ4NqKn"
       // sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // replace with your actual sitekey
       onChange={onChange}
     />
